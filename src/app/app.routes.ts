5f34bde32eef355ceb6915d5da3ec764f1955c23
Deepak Sisodiya <deepak.sisodiya@gmail.com>
2024-07-26 12:42:08 +0530

    learning angular

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { WishComponent } from './wish/wish.component';
 import { ContactComponent } from './contact/contact.component';
 import { NotfoundComponent } from './notfound/notfound.component';
 import { ProductlistComponent } from './products/productlist/productlist.component';
+import { ProductdetailsComponent } from './products/productdetails/productdetails.component';
 
 export const routes: Routes = [
   { path: '', component: WishComponent }, // Default route
   { path: 'first', component: WishComponent },
   { path: 'second', component: ContactComponent },
   { path: 'products', component: ProductlistComponent },
+  { path: 'products/:id', component: ProductdetailsComponent },
   { path: '**', component: NotfoundComponent }, // Wildcard route for a 404 page
 ];
